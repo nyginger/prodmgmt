d8a96734257da3b90f974af9eaf81f673d65c807
nyginger <whatsup2@gmail.com>
2018-05-16 16:31:07 +0900

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,374 @@
+# -*- coding: utf-8 -*-
+
+
+
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import BaseView, expose
+from eins_app import db
+
+
+
+
+class LimitModelView(ModelView):
+    can_delete = False  # disable model deletion
+    page_size = 10  # the number of entries to display on the list view
+    can_export=True
+
+class Works(db.Model):
+    __tablename__ = 'works'
+    id = db.Column(db.Integer,primary_key=True)
+    work_no=db.Column(db.String(80))
+    lot_id=db.Column(db.String(80))
+    work_name=db.Column(db.Unicode)
+    company=db.Column(db.String(160))
+    prod_name=db.Column(db.String(180))
+    anod_type=db.Column(db.Unicode)
+    person= db.Column(db.String(80))
+    person_count=db.Column(db.Integer)
+    prod_serial=db.Column(db.String(160))
+    in_out=db.Column(db.Unicode)
+    pub_time=db.Column(db.DateTime)
+    message=db.Column(db.String(180))
+    filename=db.Column(db.String(300))
+    filelocation=db.Column(db.String(1000))
+    data=db.Column(db.BLOB)
+    stage= db.Column(db.String(80))
+    work_no2= db.Column(db.String(80))
+    in_out2= db.Column(db.String(80))
+    com_id= db.Column(db.Integer)
+
+    def __repr__(self):
+    	return self.id
+
+class Persons(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(60))
+    username = db.Column(db.String(80), nullable=False,unique=True)
+    password=db.Column(db.String(300),nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    active=db.Column(db.Boolean(),nullable=False)
+    confirmed_at=db.Column(db.DateTime())
+
+    def __repr__(self):
+        return self.username
+
+class Stage(db.Model):
+    __tablename__ = 'stage'
+    stage_id=db.Column(db.Integer, primary_key=True)
+    id=db.Column(db.Integer)
+    work=db.Column(db.String(120))
+    zone=db.Column(db.String(120))
+    zone_id=db.Column(db.String(100))
+    work_code=db.Column(db.String(60))
+    def __repr__(self):
+        return self.work
+
+class Lot(db.Model):
+    __tablename__ = 'lot'
+    id = db.Column(db.Integer, primary_key=True)
+    lot_id= db.Column(db.String(80))
+    com_id=db.Column(db.Integer, db.ForeignKey('company.id'))
+    com_name=db.relationship("Company", backref=db.backref('lot'), lazy=True)
+    company=db.Column(db.String(160))
+    counts=db.Column(db.Integer)
+    person= db.Column(db.String(80))
+    gen= db.Column(db.String(80))
+    prod_type=db.Column(db.Integer, db.ForeignKey('product.prod_id'))
+    prod_category= db.Column(db.String(80))
+    #prod_category= db.relationship("Product", backref=db.backref('lot'), lazy=True)
+    prod_name= db.Column(db.String(180))
+    prod_serial= db.Column(db.String(80))
+    anod_type= db.Column(db.String(80))
+    pol_YN= db.Column(db.String(80))
+    mask_YN= db.Column(db.String(80))
+    shil_YN= db.Column(db.String(80))
+    shil_type= db.Column(db.String(80))
+    surf_layer= db.Column(db.String(80))
+    RA= db.Column(db.String(80))
+    cur_work=db.Column(db.String(80))
+    cur_zone=db.Column(db.String(80))
+    pre_work=db.Column(db.String(80))   
+    pre_zone=db.Column(db.String(80))
+    next_work=db.Column(db.String(80))
+    next_zone=db.Column(db.String(80))
+    cur_progress=db.Column(db.Integer)
+    in_date=db.Column(db.DateTime)
+    target_date=db.Column(db.DateTime)
+    proc1=db.Column(db.String(80))
+    proc2=db.Column(db.String(80))
+    proc3=db.Column(db.String(80))
+    proc4=db.Column(db.String(80))
+    proc5=db.Column(db.String(80))
+    proc6=db.Column(db.String(80))
+    proc7=db.Column(db.String(80))
+    proc8=db.Column(db.String(80))
+    proc9=db.Column(db.String(80))
+    proc10=db.Column(db.String(80))
+    proc11=db.Column(db.String(80))
+    proc12=db.Column(db.String(80))
+    proc13=db.Column(db.String(80))
+    proc14=db.Column(db.String(80))
+    proc15=db.Column(db.String(80))
+    proc16=db.Column(db.String(80))
+    proc17=db.Column(db.String(80))
+    proc18=db.Column(db.String(80))
+    proc19=db.Column(db.String(80))
+    proc20=db.Column(db.String(80))
+    proc21=db.Column(db.String(80))
+    proc22=db.Column(db.String(80))
+    proc23=db.Column(db.String(80))
+    proc24=db.Column(db.String(80))
+    proc25=db.Column(db.String(80))
+    proc26=db.Column(db.String(80))
+    proc27=db.Column(db.String(80))
+    proc28=db.Column(db.String(80))
+    proc29=db.Column(db.String(80))
+    proc30=db.Column(db.String(80))
+    proc31=db.Column(db.String(80))
+    proc32=db.Column(db.String(80))
+    proc33=db.Column(db.String(80))
+    proc34=db.Column(db.String(80))
+    proc35=db.Column(db.String(80))
+    proc36=db.Column(db.String(80))
+    proc37=db.Column(db.String(80))
+    proc38=db.Column(db.String(80))
+    proc39=db.Column(db.String(80))
+    proc40=db.Column(db.String(80))
+    proc41=db.Column(db.String(80))
+    proc42=db.Column(db.String(80))
+    proc43=db.Column(db.String(80))
+    proc44=db.Column(db.String(80))
+    proc45=db.Column(db.String(80))
+   
+    def __str__(self):
+        return self.lot_id
+
+
+class Stage_procs(db.Model):
+    __tablename__ = 'stage_procs'
+    id = db.Column(db.Integer, primary_key=True)
+    company=db.Column(db.String(80))
+    product= db.Column(db.String(80))
+    proc01=db.Column(db.String(80))
+    proc02=db.Column(db.String(80))
+    proc03=db.Column(db.String(80))
+    proc04=db.Column(db.String(80))
+    proc05=db.Column(db.String(80))
+    proc06=db.Column(db.String(80))
+    proc07=db.Column(db.String(80))
+    proc08=db.Column(db.String(80))
+    proc09=db.Column(db.String(80))
+    proc10=db.Column(db.String(80))
+    proc11=db.Column(db.String(80))
+    proc12=db.Column(db.String(80))
+    proc13=db.Column(db.String(80))
+    proc14=db.Column(db.String(80))
+    proc15=db.Column(db.String(80))
+    proc16=db.Column(db.String(80))
+    proc17=db.Column(db.String(80))
+    proc18=db.Column(db.String(80))
+    proc19=db.Column(db.String(80))
+    proc20=db.Column(db.String(80))
+    proc21=db.Column(db.String(80))
+    proc22=db.Column(db.String(80))
+    proc23=db.Column(db.String(80))
+    proc24=db.Column(db.String(80))
+    proc25=db.Column(db.String(80))
+        
+    def __str__(self):
+        return self.product
+
+
+class Anod(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    anod_type= db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return self.anod_type
+
+class Company(db.Model):
+    __tablename__ = 'company'
+    id = db.Column(db.Integer, primary_key=True)
+    com_name= db.Column(db.String(120), nullable=False)
+    
+    def __str__(self):
+        return self.com_name
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    prod_no = db.Column(db.Integer, primary_key=True)
+    prod_name= db.Column(db.String(120), nullable=False)
+    prod_id = db.Column(db.Integer)
+    def __str__(self):
+        return self.prod_name
+
+
+
+class Gen(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    prod_gen= db.Column(db.String(80), nullable=False)
+
+    def __str__(self):
+        return self.prod_gen
+
+
+class LotView(ModelView):
+    can_set_page_size = True
+    can_create = True
+    can_delete=False
+    create_modal = True
+    edit_modal = True
+    can_export=True
+    can_view_details = True
+    export_types = ['xlsx']
+    column_searchable_list = ['lot_id', 'prod_name','person' ,'prod_serial','in_date']
+    column_export_exclude_list= ['company','cur_work', 'cur_zone','pre_work', 'pre_zone','next_work', 'next_zone','cur_progress','proc1','proc2','proc3',
+                                'proc4','proc5','proc6','proc7','proc8','proc9','proc10','proc11','proc12','proc13','proc14','proc15','proc16','proc17','proc18',
+                                'proc19','proc20','proc21','proc22','proc23','proc24','proc25','proc26','proc27','proc28','proc29','proc30','proc31','proc32',
+                                'proc33','proc34','proc35','proc36','proc37','proc38','proc39','proc40','proc41','proc42','proc43','proc44','proc45']
+    column_exclude_list= ['company','cur_work', 'cur_zone','pre_work', 'pre_zone','next_work', 'next_zone','cur_progress','proc1','proc2','proc3',
+                                'proc4','proc5','proc6','proc7','proc8','proc9','proc10','proc11','proc12','proc13','proc14','proc15','proc16','proc17','proc18',
+                                'proc19','proc20','proc21','proc22','proc23','proc24','proc25','proc26','proc27','proc28','proc29','proc30','proc31','proc32',
+                                'proc33','proc34','proc35','proc36','proc37','proc38','proc39','proc40','proc41','proc42','proc43','proc44','proc45']
+    column_labels = {'lot_id':'LOT ID', 'com_name': '업체','prod_category': '품목 종류','counts': '수량','person':'담당자','gen': '세대', 
+                    'prod_name':'품목', 'prod_serial': '제품 시리얼','anod_type': '아노다이징 종류','pol_YN': '폴리싱 유무',
+                    'mask_YN': '마스킹 유무','shil_YN': '실링유무','shil_type': '실링 종류','surf_layer': '막두께',
+                    'RA': 'RA','cur_work':'현재 공정', 'cur_zone':'현재 공정위치','pre_work':'이전 공정','pre_zone':'이전 공정위치',
+                    'next_work':'다음 공정','next_zone':'다음 공정위치', 'in_date': '입고일','target_date': '납기일'}
+    #column_formatters = = dict(company=lambda v, c, m, p: m.com_name) # `view` is current administrative view
+    # `context` is instance of jinja2.runtime.Context
+    # `model` is model instance
+    # `name` is property name
+    from datetime import date
+    from flask_admin.model import typefmt
+
+    def date_format(view, value):
+        return value.strftime('%Y-%m-%d')
+
+    MY_DEFAULT_FORMATTERS = dict(typefmt.BASE_FORMATTERS)
+    MY_DEFAULT_FORMATTERS.update({
+            type(None): typefmt.null_formatter,
+            date: date_format
+        })
+    
+    column_type_formatters = MY_DEFAULT_FORMATTERS
+
+    form_excluded_columns = ['company','cur_work', 'cur_zone','pre_work', 'pre_zone','next_work', 'next_zone','cur_progress','proc1','proc2','proc3',
+                                'proc4','proc5','proc6','proc7','proc8','proc9','proc10','proc11','proc12','proc13','proc14','proc15','proc16','proc17','proc18',
+                                'proc19','proc20','proc21','proc22','proc23','proc24','proc25','proc26','proc27','proc28','proc29','proc30','proc31','proc32',
+                                'proc33','proc34','proc35','proc36','proc37','proc38','proc39','proc40','proc41','proc42','proc43','proc44','proc45']
+    form_args = {
+            'com_name' :{
+                'label':'업체'
+
+            },
+            'prod_category': {
+                'label': '품목 종류'
+            },
+            'counts': {
+                'label': '수량'
+            },
+            'person': {
+                'label': '담당자'
+            },
+            'gen': {
+                'label': '세대'
+            },
+            'prod_name': {
+                'label': '품목'
+            },
+            'prod_serial': {
+                'label': '제품 시리얼'
+            },
+            'anod_type': {
+                'label': '아노다이징 종류'
+            },
+            'pol_YN': {
+                'label': '폴리싱 유무'
+            },
+            'mask_YN': {
+                'label': '마스킹 유무'
+            },
+            'shil_YN': {
+                'label': '실링유무'
+            },
+            'shil_type': {
+                'label': '실링 종류'
+            },
+            'surf_layer': {
+                'label': '막두께'
+            },
+            'RA': {
+                'label': 'RA'
+            },
+            'in_date': {
+                'label': '입고일'
+            },
+            'target_date': {
+                'label': '납기일'
+            }
+
+        }
+
+
+
+class WorkView(ModelView):
+    column_searchable_list= ['lot_id','company', 'prod_name','person' ,'prod_serial','in_out','pub_time']
+    column_export_exclude_list=['filelocation','data','stage','work_no2','in_out2','com_id']
+    column_exclude_list=['filelocation','data','stage','work_no2','in_out2','com_id']
+    column_labels = { 'work_no':'작업번호','company':  '업체','work_name':'공정','prod_name':  '제품','anod_type':'아노다이징',
+                       'person':'작업자','person_count':'작업인원','prod_serial':'제품시리얼번호','in_out':'투입/완료',
+                       'pub_time':'입력시간','message':'작업메모','filename': '사진파일'}
+    can_create=True
+    can_delete=False
+    can_export=True
+    create_modal=True
+    edit_modal=True
+    can_view_details=True
+    can_set_page_size = True
+    export_types = ['xlsx']
+    form_excluded_columns=['filelocation','data','stage','work_no2','in_out2','com_id']
+    form_args = {
+                'work_no': {
+                    'label': '작업번호'
+                },
+                'company': {
+                    'label': '업체'
+                },
+                'work_name':{
+                    'label': '공정'
+                },
+                'prod_name': {
+                    'label': '제품'
+                },
+                'anod_type': {
+                    'label': '아노다이징'
+                },
+                'person': {
+                    'label': '작업자'
+                },
+                'person_count': {
+                    'label': '작업인원'
+                },
+                'prod_serial': {
+                    'label': '제품시리얼번호'
+                },
+                'in_out': {
+                    'label': '투입/완료'
+                },
+                'pub_time': {
+                    'label': '입력시간'
+                },
+                'message': {
+                    'label': '작업메모'
+                },
+                'filename': {
+                    'label': '사진파일'
+                }
+            }
+
+
---
+++ models/update_data.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+
+
+from flask_restless import APIManager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import BaseView, expose
+from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
+from flask_mail import Mail
+import os
+from eins_app import app,db, APP_ROOT
+from models import LimitModelView,Works,Persons,Stage,Lot,Stage_procs, \
+                    Anod,Company,Product,Gen,LotView,WorkView
+from sqlalchemy import and_,or_, alias, not_
+import sqlite3
+import csv
+import codecs
+import datetime
+
+conn=sqlite3.connect(APP_ROOT+os.sep+'DATABASE.db')
+cur=conn.cursor()
+
+def ins_workdata(filename):
+    f=codecs.open(APP_ROOT +os.sep+ filename,'r', encoding='euc-kr', errors='ignore')
+    data=csv.reader(f)
+    for row in data:
+        lot_id=row[1]
+        company=row[2]
+        print(company)
+        prod_name=row[3]
+        work_name=row[4]
+        in_out=row[5]
+        try:
+            pub_time=datetime.datetime.strptime(row[6], '%Y-%m-%d %H:%M')
+        except:
+            pub_time=''
+        try:
+            person_count=int(row[9])
+        except:
+            person_count=0
+        person=row[10]
+        prod_serial=row[11]
+
+        lot=Lot.query.filter(Lot.lot_id==lot_id).first()
+        print(lot)
+        if lot is None:
+            com=None
+        else:
+            com=Company.query.filter(Company.com_name.contains(lot.company)).first()
+        if com is None:
+            com=Company.query.filter(Company.com_name.contains(company)).first()
+            if com is None:
+                com_id=""
+        else:
+            com_id=com.id
+        try:
+            if prod_name=='':
+                prod_name=lot.prod_name
+        except:
+            prod_name=''
+
+        work=Stage.query.filter(Stage.work==work_name).first()
+        if work is None:
+            work=Stage.query.filter(Stage.id==988).first()
+        stg=work.id
+        stage= '%03d' % stg
+        zone=work.zone_id
+        zone_name=work.zone
+        if in_out =='투입' : io = '0'
+        else: io = '1'
+        work_no_list=[lot_id,'_',stage]
+        lot_stage= ''.join(work_no_list)
+        try:
+            pre_workno=Works.query.filter(Works.work_no2.like(lot_stage + '%')).order_by(Works.work_no2.desc()).first()
+        except:
+            pre_workno=None
+        if pre_workno is None:
+            work_no_list.extend(['01','_',io])
+        else:
+            if str(pre_workno.work_no)[-1:]!= io :
+                if io=='1':
+                    work_no_list.extend([str(pre_workno.work_no)[-4:-2],'_','1'])
+                else:
+                    n= int(str(pre_workno.work_no)[-4:-2]) + 1
+                    work_no_list.extend([ '%02d' % n,'_','0'])
+            else:
+                n= int(str(pre_workno.work_no)[-4:-2]) + 1
+                work_no_list.extend([ '%02d' % n,'_',io])
+        work_no = ''.join(work_no_list)
+        work_no2 = work_no[:-2]
+
+        #work=Works(lot_id=lot_id, work_name=work_name, work_no=work_no, work_no2=work_no2, company=company, prod_name=prod_name, stage=stage, com_id=com_id, in_out=in_out, person=person, person_count=person_count, in_out2=io, pub_time=pub_time)
+        #db.session.add(work)
+        #db.session.commit()
+        
+        sql = '''insert into works(lot_id,company,prod_name,work_name,work_no,work_no2,stage,com_id,in_out,in_out2,pub_time,
+        			person_count,person,prod_serial) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
+        cur.execute(sql,(lot_id,company,prod_name,work_name,work_no,work_no2,stage,com_id,in_out,io,pub_time,person_count,person,prod_serial))
+        conn.commit()
+        print('Data:', work_no, 'complete.')
+    print('Data uploading completed!')
+    f.close()
+
+def ins_lotdata(filename):
+    file=codecs.open(APP_ROOT +os.sep+ filename,'r', encoding='euc-kr', errors='ignore')
+    data=csv.reader(file)
+    for row in data:
+        in_date=datetime.datetime.strptime(row[1], '%Y-%m-%d')
+        lot_id=row[2]
+        company=row[3]
+        try:
+            counts=int(row[4])
+        except:
+            counts=0
+        person=row[5]
+        gen=row[6]
+        prod_name=row[7]
+        anod_type=row[8]
+        pol_YN=row[9]
+        mask_YN=row[10]
+        shil_YN=row[11]
+        shil_type=row[12]
+        surf_layer=row[13]
+        RA=row[14]
+        prod_serial=row[15]
+        target_date=datetime.datetime.strptime(row[16], '%Y-%m-%d')
+        cur_work=row[17]
+        try:
+            com=Company.query.filter(Company.com_name.contains(company)).first()
+            com_id=com.id
+        except:
+            com_id=""
+        try:
+            stage=Stage.query.filter(Stage.work==cur_work).first()
+        except:
+            stage=Stage.query.filter(Stage.zone==cur_work).first()
+        if stage is None:
+            stage=Stage.query.filter(Stage.id==988).first()
+        cur_zone=stage.zone
+        
+        sql = '''insert into lot(in_date,lot_id,company,counts,person,gen,prod_name,anod_type,pol_YN,mask_YN,shil_YN,shil_type,
+                surf_layer,RA,prod_serial,target_date,cur_work,cur_zone,com_id) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
+        cur.execute(sql,(in_date,lot_id,company,counts,person,gen,prod_name,anod_type,pol_YN,mask_YN,shil_YN,shil_type,surf_layer,RA,prod_serial,target_date,cur_work,cur_zone,com_id))
+        conn.commit()
+        print('Data:', lot_id, 'complete.')
+    print('Data uploading completed!')
+    file.close()
+
+
+
+def update_prodtype():
+    compro_list=[{'prod_name': ['LINER','이중월','이중_WALL','WALL_LINER'],'gen':['A3','6G'],'others':0}]
+    compro_list.append({'prod_name': ['DRY GAMMA'],'gen':[],'others':0})
+    compro_list.append({'prod_name': ['LINER','이중월','이중_WALL','WALL_LINER'],'gen':['A3','6G'],'others':1})
+    compro_list.append({'prod_name':['DIFFUSER','D/F'],'gen':['A3','6G'],'others':0})
+    compro_list.append({'prod_name':['DIFFUSER','D/F'],'gen':['A2','5.5G'],'others':0})
+    compro_list.append({'prod_name':['DIFFUSER','D/F'],'gen':['A3','6G','A2','5.5G'],'others':1})
+    compro_list.append({'prod_name':['SUSCEPTOR','S/T'],'gen':['A3','6G'],'others':0})
+    compro_list.append({'prod_name':['SUSCEPTOR','S/T'],'gen':['A2','5.5G'],'others':0})
+    compro_list.append({'prod_name':['SUSCEPTOR','S/T'],'gen':['A3','6G','A2','5.5G'],'others':1})
+    compro_list.append({'prod_name':['SHADOW_FRAME','SHADOWFRAME','S/F'],'gen':['A3','6G'],'others':0})
+    compro_list.append({'prod_name':['SHADOW_FRAME','SHADOWFRAME','S/F'],'gen':['A2','5.5G'],'others':0})
+    compro_list.append({'prod_name':['SHADOW_FRAME','SHADOWFRAME','S/F'],'gen':['A3','6G','A2','5.5G'],'others':1})
+    compro_list.append({'prod_name':['하부전극'],'gen':[],'others':0})
+    compro_list.append({'prod_name': ['TRAY_PLATE'],'gen':[],'others':0})
+    compro_list.append({'prod_name': ['SHOWER_HEAD'],'gen':[],'others':0})
+    compro_list.append({'prod_name': ['RWD'],'gen':[],'others':0})
+    compro_list.append({'prod_name': ['SCE'],'gen':[],'others':0})
+    
+    
+   
+    cur.execute("update lot set prod_category ='기타_제품' ")
+    conn.commit()
+    
+    lot=Lot.query.subquery()
+    dt_lot=[]
+    for compro in compro_list:
+        if compro['others']==0 :
+            sublot=lot
+            try:
+                com_prod=compro['gen'][0]
+            except:
+                com_prod=''
+            if compro['gen']!=[]:
+                gens=[]
+                for gen in compro['gen']:
+                    gens.append(sublot.c.gen.contains(gen))
+                sublot = db.session.query(sublot).filter(or_(*gens)).subquery()
+            prods = []
+            for name in compro['prod_name']:
+                prods.append(sublot.c.prod_name.contains(name))
+            sublot = db.session.query(sublot).filter(or_(*prods)).subquery()
+            if compro['gen']==[]:
+                com_prod+='' + compro['prod_name'][0].replace(' ','_')
+            else:
+                com_prod+='_' + compro['prod_name'][0].replace(' ','_')
+
+            lot_work=db.session.query(sublot).all()
+            lot_cnt=db.session.query(sublot).count()
+            dt_lot.append({'prod':com_prod,'lot_id':[r.lot_id for r in lot_work],'count':lot_cnt})
+
+    
+        elif compro['others']==1:
+            otherlot=lot
+            prods = []
+            for name in compro['prod_name']:
+                prods.append(otherlot.c.prod_name.contains(name))
+            sublot = db.session.query(otherlot).filter(or_(*prods)).subquery()
+            gens=[]
+            for gen in compro['gen']:
+                gens.append(~sublot.c.gen.contains(gen))
+            sublot = db.session.query(sublot).filter(and_(*gens)).subquery()
+
+            lot_work=db.session.query(sublot).all()
+            com_prod= compro['prod_name'][0] + '_기타'
+            
+            dt_lot.append({'prod':com_prod,'lot_id':[r.lot_id for r in lot_work]})
+        else: 
+            otherlot2=lot
+            prods = []
+            for name in compro['prod_name']:
+                prods.append(~otherlot2.c.prod_name.contains(name))
+            sublot = db.session.query(otherlot2).filter(and_(*prods)).subquery()
+
+            lot_work=db.session.query(sublot).all()
+            com_prod='기타_제품'
+            dt_lot.append({'prod':com_prod,'lot_id':[r.lot_id for r in lot_work]})
+
+   
+    return dt_lot
+
+
+def update_lot_prod(lot_id,prod_cat):
+    try:
+        sql='update lot set prod_category =? where lot_id=?'
+        cur.execute(sql,(prod_cat,lot_id))
+        conn.commit()
+        print('Lot ID:', lot_id,'Product', prod_cat, 'complete.')
+        
+    
+        return 1
+    except:
+        return 0
