0afbaeac7dd017989376fdf7a3e1ff12180dbe6f
nyginger <whatsup2@gmail.com>
2018-05-16 16:26:17 +0900

    Update admin_view.py

--- views/admin_view.py
+++ views/admin_view.py
@@ -8,7 +8,7 @@ from flask_admin import BaseView, expose
 #from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
 from flask_mail import Mail
 import os
-from eins_app import app,db
+from prod_app import app,db
 from models import LimitModelView,Works,Persons,Stage,Lot,Stage_procs, \
                     Anod,Company,Product,Gen,LotView,WorkView
 
